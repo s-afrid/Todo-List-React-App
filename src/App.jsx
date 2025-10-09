67af5a747dd2a0052188fc96cffea7dc9d46f5a1
s-afrid <afrid0052@gmail.com>
2025-10-09 09:59:26 +0530

    added a list of todos and added add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,54 @@ import Navbar from './components/Navbar'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, settodo] = useState("")
+  const [todos, settodos] = useState([])
+  const handleEdit = ()=>{
+
+  }
+
+    const handleDelete = ()=>{
+    
+  }
+
+  const handleChange = (e)=>{
+    settodo(e.target.value)
+  } 
+
+  const handleAdd = ()=>{
+    settodos([...todos, {todo, isCompleted: false}])
+    settodo("")
+  }
 
   return (
     <>
       <Navbar/>
+      <div className="container mx-auto my-5 rounded-xl bg-[#edeae1] p-5 min-h-[70vh]">
+        <div className="addTodo my-5">
+          <h2 className='text-lg font-bold'>Add a todo</h2>
+          <input onChange={handleChange} value={todo} type="text" className='w-1/2'/>
+        <button onClick={handleAdd} className='bg-green-800 md:hover:bg-green-700 active:bg-green-700 p-3 py-1 rounded-lg text-white transition-all duration-200 mx-6 text-sm font-bold'>Add</button>
+        </div>
+        
+          <h1 className='text-xl font-bold'>Your Todos</h1>
+
+          <div className="todos">
+            {todos.map(item=>{
+              return (
+               <div key={item.todo} className="todo flex w-1/2 justify-between my-3">
+              <div className={item.isCompleted?"":"line-through"}>
+                {item.todo}
+              </div>
+              <div className="buttons">
+                <button onClick={handleEdit} className='bg-green-800 md:hover:bg-green-700 active:bg-green-700 p-3 py-1 rounded-lg text-white transition-all duration-200 mx-2 text-sm font-bold'>Edit</button>
+                <button onClick={handleDelete} className='bg-green-800 md:hover:bg-green-700 active:bg-green-700 p-3 py-1 rounded-lg text-white transition-all duration-200 mx-2 text-sm font-bold'>Delete</button>
+              </div>
+          </div>
+              )
+            })}
+           
+      </div>
+      </div>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between bg-amber-500 text-white py-2'>
+    <nav className='flex justify-between bg-[#166c96] text-white py-2'>
         <div className="logo">
             <span className='font-bold text-xl mx-8'>Task Nest</span>
         </div>
