7e864eb4296725292894e07a548eacd902cf006f
s-afrid <afrid0052@gmail.com>
2025-10-14 11:41:19 +0530

    installed react icons

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     }
   }, [])
   
-  const saveToLs = () => {
+  const saveToLs = (params) => {
     localStorage.setItem("todos",JSON.stringify(todos))
   }
   const toggleFinished = (e) => {
@@ -26,9 +26,12 @@ function App() {
   
   
   const handleEdit = (e, id)=>{
-    let t = todos.filter(i=>i.id===id)
+    let t = todos.filter(i=>i.id === id)
     settodo(t[0].todo)
-    handleDelete(e, id)
+    let newTodos = todos.filter(item=>{
+      return item.id!==id
+    }); 
+    settodos(newTodos) 
     saveToLs()
   }
 
@@ -49,7 +52,7 @@ function App() {
     settodos([...todos, {id: uuidv4(),todo, isCompleted: false}])
     settodo("")
     saveToLs()
-    saveToLs()
+    
   }
 
   const handleCheckBox = (e) => {
@@ -69,14 +72,15 @@ function App() {
   return (
     <>
       <Navbar/>
-      <div className="container mx-auto my-5 rounded-xl bg-[#edeae1] p-5 min-h-[70vh]">
-        <div className="addTodo my-5">
+      <div className="container mx-auto my-5 rounded-xl bg-[#edeae1] p-5 min-h-[70vh] w-1/2">
+      <h1 className='font-bold text-center text-xl'>Task Nest - Manage all your todos at one place</h1>
+        <div className="addTodo my-5 flex flex-col gap-3 items-center">
           <h2 className='text-lg font-bold'>Add a todo</h2>
-          <input onChange={handleChange} value={todo} type="text" className='w-1/2 bg-white'/>
-        <button onClick={handleAdd} disabled={todo.length<=3} className='bg-[#cd2028] md:hover:bg-[#eb404e] active:bg-green-700 p-3 py-1 rounded-lg text-white transition-all duration-200 mx-6 text-sm font-bold disabled:bg-gray-500'>Save</button>
+          <input onChange={handleChange} value={todo} type="text" className='w-full bg-white'/>
+        <button onClick={handleAdd} disabled={todo.length<=3} className='w-1/2 bg-[#cd2028] md:hover:bg-[#eb404e] active:bg-green-700 p-3 py-1 rounded-lg text-white transition-all duration-200 mx-6 text-sm font-bold disabled:bg-gray-500 disabled:hover:bg-gray-600'>Save</button>
         </div>
         
-          <input onChange={toggleFinished} type="checkbox" checked={showFinished} name="" id="" />
+          <input onChange={toggleFinished} type="checkbox" checked={showFinished} id="show" className='my-4' />
           <label className='mx-2' htmlFor="show">Show Finished</label> 
 
           <h1 className='text-xl font-bold'>Your Todos</h1>
@@ -85,9 +89,9 @@ function App() {
             {todos.length === 0 && <div className='m-5'>No task to display</div>}
             {todos.map(item=>{
               return (
-               (showFinished || !item.isCompleted) && <div key={item.id} className="todo flex w-1/2 justify-between my-3">
+               (showFinished || !item.isCompleted) && <div key={item.id} className={"todo flex w-1/2 justify-between my-3"}>
                 <div className='flex gap-5 items-center'>
-                <input onChange={handleCheckBox} type="checkbox" checked={todo.isCompleted} name={item.id} id="" />
+                <input name={item.id} onChange={handleCheckBox} type="checkbox" checked={todo.isCompleted} id="" />
               <div className={item.isCompleted?"line-through":""}>    
                 {item.todo}
                 </div>
